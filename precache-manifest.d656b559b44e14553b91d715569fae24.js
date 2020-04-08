c7c01e1aab25122e91587ed8c34bbbd6e23965ca
Valeriy Samoilov <valerasam1012@gmail.com>
2020-04-08 03:05:02 +0300

    Updates

---
+++ precache-manifest.d656b559b44e14553b91d715569fae24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08b3ef8ff16bfa698f7e73e5f7432ce",
+    "url": "/react-pokedex-app/index.html"
+  },
+  {
+    "revision": "66e011ea573d43327431",
+    "url": "/react-pokedex-app/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "45f247cdfe1a7fbca234",
+    "url": "/react-pokedex-app/static/css/main.43aee519.chunk.css"
+  },
+  {
+    "revision": "66e011ea573d43327431",
+    "url": "/react-pokedex-app/static/js/2.da138672.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-pokedex-app/static/js/2.da138672.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45f247cdfe1a7fbca234",
+    "url": "/react-pokedex-app/static/js/main.5fc68650.chunk.js"
+  },
+  {
+    "revision": "28543f1bfe0d3b181b69",
+    "url": "/react-pokedex-app/static/js/runtime-main.20e49249.js"
+  }
+]);
