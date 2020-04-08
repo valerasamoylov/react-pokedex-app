bdf9df6a68d935c2205b104eff7732b5de1f2285
Valeriy Samoilov <valerasam1012@gmail.com>
2020-04-08 04:12:51 +0300

    fixed link in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,9 @@ function Header() {
   return (
     <nav className="menu" style={{ zIndex: 1 }}>
       <div className="menu__logo">
-        <a href="/">Pokedex</a>
+        <a href="https://valerasamoylov.github.io/react-pokedex-app/">
+          Pokedex
+        </a>
       </div>
     </nav>
   );
